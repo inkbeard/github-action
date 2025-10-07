2241a52646e6bcbd55d95439461d4b263788c5ed
Aaron Stoddart <aaron@inkbeard.com>
2025-10-06 21:31:47 -0500

    migrate jest to vitest

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -5,13 +5,13 @@
  * functions and objects. For example, the core module is mocked in this test,
  * so that the actual '@actions/core' module is not imported.
  */
-import { jest } from '@jest/globals'
+import { vi } from 'vitest'
 import * as core from '../__fixtures__/core.js'
 import { wait } from '../__fixtures__/wait.js'
 
 // Mocks should be declared before the module being tested is imported.
-jest.unstable_mockModule('@actions/core', () => core)
-jest.unstable_mockModule('../src/wait.js', () => ({ wait }))
+vi.mock('@actions/core', () => core)
+vi.mock('../src/wait.js', () => ({ wait }))
 
 // The module being tested should be imported dynamically. This ensures that the
 // mocks are used in place of any actual dependencies.
@@ -27,7 +27,7 @@ describe('main.ts', () => {
   })
 
   afterEach(() => {
-    jest.resetAllMocks()
+    vi.resetAllMocks()
   })
 
   it('Sets the time output', async () => {
